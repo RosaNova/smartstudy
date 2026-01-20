df2b07d1639167098e0b8bac2e1144567004270b
r_sunrise <rozasun61@gmail.com>
2026-01-20 23:48:12 +0700

    fix

--- client/postcss.config.mjs
+++ client/postcss.config.mjs
@@ -1,7 +1,19 @@
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import postcssPresetEnv from "postcss-preset-env";
+
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+    postcssPresetEnv({
+      stage: 0, // enable experimental CSS features like @property
+      features: {
+        "custom-properties": true, // enable CSS variables
+        "nesting-rules": true
+      }
+    }),
+  ],
 };
 
 export default config;
