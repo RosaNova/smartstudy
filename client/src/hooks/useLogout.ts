efb79ba9b5bbdefb6f27b446da4beb9392fc2898
r_sunrise <rozasun61@gmail.com>
2026-01-20 23:57:49 +0700

    new update

--- client/src/hooks/useLogout.ts
+++ client/src/hooks/useLogout.ts
@@ -1,7 +1,7 @@
 import React from 'react'
 import { signOut } from "firebase/auth";
 import Cookies from "js-cookie";
-import { auth } from "@/firebase/firebaseConfig";
+// import { auth } from "@/firebase/firebaseConfig";
 import { useNavigate } from 'react-router-dom';
 
 export const useLogout = () => {
